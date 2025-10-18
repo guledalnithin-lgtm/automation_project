1976ebafb58e1da4f95e84ba64a0716b9de11bee
guled <guled@192.168.1.200>
2025-10-18 11:30:12 +0530

    github package added

---
+++ Automation_Framework/src/test/java/githubdemo/SampleGit.java
@@ -0,0 +1,13 @@
+package githubdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleGit {
+	@Test
+	public void test() {
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
